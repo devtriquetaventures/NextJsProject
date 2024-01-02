92aa2fee6bc84fcccd6d12a32a46ee2581c6564c
devtriquetaventures <dev.triquetaventures@gmail.com>
2024-01-02 11:35:09 -0300

    TEN-123 - Cambio del header en mobile

--- app/ui/tense-mini-logo.tsx
+++ app/ui/tense-mini-logo.tsx
@@ -1,15 +1,15 @@
 import Image  from 'next/image'
 
-export default function TenseLogo() {
+export default function TenseMiniLogo() {
   return (
     <div
       className={`flex flex-row items-center leading-none`}
     >
       <Image
-        src="/tense.png"
-        width={1000}
-        height={760}
-        className="hidden md:block"
+        src="/tense-logo.png"
+        width={50}
+        height={200}
+        className="block md:hidden"
         alt="Screenshots of the dashboard project showing desktop version"
       />
     </div>
