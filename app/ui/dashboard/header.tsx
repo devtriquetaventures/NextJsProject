5d172fb96843b5774819d9477809e47ed5c85a64
devtriquetaventures <dev.triquetaventures@gmail.com>
2024-01-02 11:40:29 -0300

    TEN-123 - Refactorizacion del icono del header

--- app/ui/dashboard/header.tsx
+++ app/ui/dashboard/header.tsx
@@ -2,16 +2,12 @@ import NavLinks from '@/app/ui/dashboard/nav-links';
 import TenseLogo from '@/app/ui/tense-logo';
 import DarkMode from '../darkmode';
 import UserIcon from '../user-icon';
-import TenseMiniLogo from '../tense-mini-logo';
 
 export default function Header() {
   return (
     <header className="flex flex-row md:pl-6 md:px-2 bg-gradient-to-tr from-sky-900 via-black to-purple-900">
-        <div className="md:w-24 flex justify-center justify-items-center ml-2">
+        <div className="md:w-24 flex justify-center justify-items-center ml-4 md:ml-2">
           <TenseLogo />
-        </div>
-        <div className="md:hidden flex justify-center justify-items-center ml-2">
-          <TenseMiniLogo />
         </div>
       <div className="flex h-20 grow flex-row justify-start justify-items-center">
         <div className='flex justify-start justify-items-center pt-5 pl-8'>
